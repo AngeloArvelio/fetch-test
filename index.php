475326f57180a92331c97432cdc3c310258c6fbe
AngeloArvelio <realembernite@gmail.com>
2025-02-18 11:37:32 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate perspiciatis natus quo ipsa quos, error dolores itaque consequatur quod ut libero tempore recusandae illo qui adipisci, distinctio obcaecati vel molestias?</p>
 </body>
 </html>
